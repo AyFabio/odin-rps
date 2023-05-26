caa196bf7c242be9d5bd726e74a67c27ba0b632f
ayfabio <127374141+AyFabio@users.noreply.github.com>
2023-05-26 17:31:58 -0400

    Add game function to play game 5 times.

--- script.js
+++ script.js
@@ -13,28 +13,55 @@ function getComputerChoice() {
 
 
 //user choice is compared to computer choice and winner is chosen.
-function playRound(playerChoice, computerChoice){
+function playRound(playerChoice = prompt('Please choose rock, paper, or scissors.'), computerChoice = getComputerChoice()){
 
     if (playerChoice.toLowerCase() === computerChoice.toLowerCase()) {
-        return "It\'s a Draw!"
+        console.log(`You both chose ${computerChoice}, It\'s a draw!`)
+        return 2;
     } else if (playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === 'paper') {
-        return `Paper beats Rock. You Lose!`
+        console.log(`Paper beats Rock. You Lose!`)
+        return 0;
     } else if (playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === 'scissors') {
-        return `Rock beats Scissors. You Win!`
+        console.log(`Rock beats Scissors. You Win!`)
+        return 1;
     } else if (playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === 'rock') {
-        return `Paper beats Rock. You Win!`
+        console.log(`Paper beats Rock. You Win!`)
+        return 1;
     } else if (playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === 'scissors') {
-        return `Scissors beats Paper. You Lose!`
+        console.log(`Scissors beats Paper. You Lose!`)
+        return 0;
     } else if (playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === 'rock') {
-        return `Rock beats Scissors. You Lose!`
+        console.log(`Rock beats Scissors. You Lose!`)
+        return 0;
     } else if (playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === 'paper') {
-        return `Scissors beats Paper. You Win!`
+        console.log(`Scissors beats Paper. You Win!`)
+        return 1;
     } else {
-        return 'You entered an invalid choice! Please enter Rock, Paper, or Scissors!'
+        console.log('You entered an invalid choice! Please enter Rock, Paper, or Scissors!')
+        return 3;
     }
 }
 
 function game(){
-    
+    let userScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++){
+        let roundResult = playRound();
+        if (roundResult === 1){
+            userScore ++;        
+        } else if (roundResult === 0) {
+            computerScore ++;
+        } else if (roundResult === 3){
+            i --;
+        }
+    }
+   
+    if (userScore > computerScore) {
+        console.log(`The score is User:${userScore} Computer:${computerScore}.You win!`)
+    } else if ( userScore === computerScore ) {
+        console.log(`The score is User:${userScore} Computer:${computerScore}. It\'s a tie!`)
+    } else {
+        console.log(`The score is User:${userScore} Computer:${computerScore}.You lose!`)
+    }
 }
 //game will be played 5 times with winner declared at the end.
