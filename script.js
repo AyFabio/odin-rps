97a5225ebf2a876de9f465a6277c1aaafb2fe6a4
ayfabio <127374141+AyFabio@users.noreply.github.com>
2023-05-26 16:18:00 -0400

    Add function to get computer choice and winner

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 //computer randomly chooses rock, paper or scissors
-//user inputs rock, paper, or scissors. 
-//user choice is compared computer choice and winner is chosen.
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random()*3);
+
+    if (randomInt === 0) {
+        return "Rock";
+    } else if (randomInt === 1) {
+    return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+
+//user choice is compared to computer choice and winner is chosen.
+function playRound(playerChoice, computerChoice){
+
+    if (playerChoice.toLowerCase() === computerChoice.toLowerCase()) {
+        return "It\'s a Draw!"
+    } else if (playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === 'paper') {
+        return `Paper beats Rock. You Lose!`
+    } else if (playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === 'scissors') {
+        return `Rock beats Scissors. You Win!`
+    } else if (playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === 'rock') {
+        return `Paper beats Rock. You Win!`
+    } else if (playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === 'scissors') {
+        return `Scissors beats Paper. You Lose!`
+    } else if (playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === 'rock') {
+        return `Rock beats Scissors. You Lose!`
+    } else if (playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === 'paper') {
+        return `Scissors beats Paper. You Win!`
+    } else {
+        return 'You entered an invalid choice! Please enter Rock, Paper, or Scissors!'
+    }
+}
+
+function game(){
+    
+}
 //game will be played 5 times with winner declared at the end.
-
-
-
