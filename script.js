534915a27ea965158f5fa9a28e4dd6ae074c1c9c
ayfabio <127374141+AyFabio@users.noreply.github.com>
2023-05-26 14:06:35 -0400

    plan overall structure in js comments

---
+++ script.js
@@ -0,0 +1,7 @@
+//computer randomly chooses rock, paper or scissors
+//user inputs rock, paper, or scissors. 
+//user choice is compared computer choice and winner is chosen.
+//game will be played 5 times with winner declared at the end.
+
+
+
